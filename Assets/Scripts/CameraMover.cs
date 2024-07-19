3462c3c056890a29ef843da4c4d5598cd512a391
Vladislav <GimliVlad@list.ru>
2024-07-19 10:40:38 +0300

    Add camera clamp

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 internal class CameraMover : MonoBehaviour
 {
+    [SerializeField] private float _absolutePositionX = 186f;
+    [SerializeField] private float _absolutePositionZ = 142f;
     [SerializeField] private Transform _target;
 
     private Transform _transform;
@@ -20,6 +22,14 @@ internal class CameraMover : MonoBehaviour
     {
         _position = new Vector3(_target.position.x - _lagX, _transform.position.y, _target.position.z - _lagZ);
 
+        ClampPosition();
+        
         _transform.position = _position;
+    }
+
+    private void ClampPosition()
+    {
+        _position.x = Mathf.Clamp(_position.x, -_absolutePositionX, _absolutePositionX);
+        _position.z = Mathf.Clamp(_position.z, -_absolutePositionZ, _absolutePositionZ);
     }
 }
