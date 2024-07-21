0b6ccd5de8cd6fea920095295f0456351d15af3b
Vladislav <GimliVlad@list.ru>
2024-07-21 12:11:16 +0300

    Update health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IPlayerHealth
 {
+    private bool _isInvulnerable;
+
     public event Action PlayerDied;
 
     public void Die()
     {
+        if (_isInvulnerable)
+            return;
+
         PlayerDied?.Invoke();
+    }
+
+    public void SetInvulnerable(bool value)
+    {
+        _isInvulnerable = value;
     }
 }
