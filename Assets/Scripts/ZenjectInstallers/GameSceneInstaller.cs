5ed72f390fb38f53c80152930233f5bc38b19917
Vladislav <GimliVlad@list.ru>
2024-07-23 13:02:46 +0300

    Add zenject installers namespace

---
+++ Assets/Scripts/ZenjectInstallers/GameSceneInstaller.cs
@@ -0,0 +1,24 @@
+using Gameplay;
+using UnityEngine;
+using Zenject;
+
+namespace ZenjectInstallers
+{
+    public class GameSceneInstaller : MonoInstaller
+    {
+        [SerializeField] private MoveInput _moveInput;
+        [SerializeField] private GunChanger _gunChanger;
+        [SerializeField] private ScoreCounter _scoreCounter;
+        [SerializeField] private PlayerHealth _player;
+        [SerializeField] private TimeController _timeController;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IMoveInput>().FromInstance(_moveInput);
+            Container.Bind<IGunChanger>().FromInstance(_gunChanger);
+            Container.Bind<IScoreCounter>().FromInstance(_scoreCounter);
+            Container.Bind<IPlayerHealth>().FromInstance(_player);
+            Container.Bind<ITimeController>().FromInstance(_timeController);
+        }
+    }
+}
